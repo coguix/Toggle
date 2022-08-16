5bd9445e0f0b654cd5ce1efed197c13cfd4d5567
Rodrigo Jiménez <84687472+coguix@users.noreply.github.com>
2022-08-16 20:50:01 +0200

    Initial Commit

---
+++ Toggle/ToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleApp.swift
+//  Toggle
+//
+//  Created by Rodrigo Jiménez Guardiola on 16/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
