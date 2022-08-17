ee037c6fa397116c5217a643405bc998ad0f3d8e
Rodrigo Jiménez <84687472+coguix@users.noreply.github.com>
2022-08-17 20:04:45 +0200

    Create awesome text

---
+++ Toggle/AwsomeTextView.swift
@@ -0,0 +1,20 @@
+//
+//  AwsomeTextView.swift
+//  Toggle
+//
+//  Created by Rodrigo Jiménez Guardiola on 17/8/22.
+//
+
+import SwiftUI
+
+struct AwsomeTextView: View {
+    var body: some View {
+        Text("Awesome text")
+    }
+}
+
+struct AwsomeTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwsomeTextView()
+    }
+}
